9d323dbe2fb416da819d80409ba69848784e89f0
artemtor <151242343+artemtor@users.noreply.github.com>
2023-11-18 09:51:49 +0300

    Delete cashdesk.kt

--- cashdesk.kt
+++
@@ -1,8 +0,0 @@
-import kotlin.random.Random
-
-class CashDesk( val min: Int, val max: Int) {
-    fun sellTicket():Int {
-        val sell = Random.nextInt(min, max + 1)
-        return sell
-    }
-}
